39d8c631090c27649e767d30295542c0e5145619
IncognitoMan <thecatwithawp@gmail.com>
2022-12-09 13:50:53 +0100

    test3

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
-<!DOCTYPE html>
+<?php
+?>
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -11,5 +13,6 @@
 <body>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script> 
   <button type="button" class="btn btn-primary">primary button</button>
+  <p>does this work?</p>
 </body>
 </html>
