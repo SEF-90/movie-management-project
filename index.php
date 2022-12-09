91a71d3f463659d2d2de63e4c5cec1193ef833bd
IncognitoMan <thecatwithawp@gmail.com>
2022-12-09 13:45:53 +0100

    Added a boostrap test button

--- index.php
+++ index.php
@@ -9,8 +9,7 @@
   </head>
 </head>
 <body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-    <h1>Sus</h1>
-    <p>test</p>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script> 
+  <button type="button" class="btn btn-primary">primary button</button>
 </body>
 </html>
